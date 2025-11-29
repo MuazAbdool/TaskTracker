c09e3de23f3d689d16e4e21251831b542c65469f
MuazAbdool <mrawenxa@gmail.com>
2025-11-29 17:13:20 +0200

    Added basic crud interface for my infrustruture

---
+++ Backend/TaskTracker/TaskTracker.Domain/Interfaces/Repositories/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using Task = Tastracker.Domain.Entities.Task;
+
+namespace Tastracker.Domain.Interfaces.Repositories;
+
+public interface ITaskRepository
+{
+    Task<Task>GetByIdAsync(int id);
+    Task<IEnumerable<Task>>GetAllAsync();
+    Task AddAsync(Task task);
+    Task UpdateAsync(Task task);
+    Task DeleteAsync(int id);
+}
